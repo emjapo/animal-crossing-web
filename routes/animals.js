eb9541561f8be6f9773b478eaa58e7fe1ad402c9
Emily <porte2@mailbox.winthrop.edu>
2020-04-25 01:54:54 -0400

    added bug routes

--- routes/animals.js
+++ routes/animals.js
@@ -11,6 +11,7 @@ const fish2 = Item.create({ name: "Guppy", image: "images/fish.png", price: "130
 const fish3 = Item.build({ name: "Horse Mackerel", image: "images/fish.png", price: "150", canDonate: "1" });
 const fish4 = Item.build({ name: "Red Snapper", image: "images/fish.png", price: "3000", canDonate: "1" });
 const fish5 =Item.build({ name: "Carp", image: "images/fish.png", price: "300", canDonate: "1" });
+
 const bug1 = Item.build({ name: "Peacock Butterfly", image: "images/bug.png", price: "2500", canDonate: "1" });
 const bug2 = Item.build({ name: "Long Locust", image: "images/bug.png", price: "200", canDonate: "1" });
 const bug3 = Item.build({ name: "Tarantula", image: "images/bug.png", price: "8000", canDonate: "1" });
@@ -18,7 +19,7 @@ const bug4 = Item.build({ name: "Ladybug", image: "images/bug.png", price: "200"
 const bug5 = Item.build({ name: "Honeybee", image: "images/bug.png", price: "200", canDonate: "1" });
 
 router.get('/river', (req, res) => {
-
+    res.render('river');
 });
 
 router.post('/river', (req, res) => {
@@ -87,6 +88,83 @@ router.post('/river', (req, res) => {
                 })
                     .catch((err) => {
                         console.log("error saving fish 5");
+                    });
+                break;
+        }
+        res.redirect('/');
+    }
+});
+
+router.get('/garden', (req, res) => {
+    res.render('garden');
+});
+
+router.post('/garden', (req, res) => {
+    req.session.pocketCount = req.session.pocketCount + 1 || 1;
+    if (req.session.pocketCount == 10) {
+        res.redirect('/pocket/full');
+    }
+    else {
+        let randNum = Math.floor((Math.random() * 10));
+        switch (randNum) {
+            case 0:
+                Villager.findByPk(req.session.user).then((villager) => {
+                    bug1.save()
+                        .then((bug) => {
+                            villager.addItem([bug])
+                        })
+                })
+                    .catch((err) => {
+                        console.log("error saving bug 1");
+                    });
+                break;
+            case 1:
+            case 2:
+                Villager.findByPk(req.session.user).then((villager) => {
+                    bug2.save()
+                        .then((bug) => {
+                            villager.addItem([bug])
+                        })
+                })
+                    .catch((err) => {
+                        console.log("error saving bug 2");
+                    });
+                break;
+            case 3:
+                Villager.findByPk(req.session.user).then((villager) => {
+                    bug3.save()
+                        .then((bug) => {
+                            villager.addItem([bug])
+                        })
+                })
+                    .catch((err) => {
+                        console.log("error saving bug 3");
+                    });
+                break;
+            case 4:
+            case 5:
+            case 6:
+                Villager.findByPk(req.session.user).then((villager) => {
+                    bug4.save()
+                        .then((bug) => {
+                            villager.addItem([bug])
+                        })
+                })
+                    .catch((err) => {
+                        console.log("error saving bug 4");
+                    });
+                break;
+            case 7:
+            case 8:
+            case 9:
+                Villager.findByPk(req.session.user).then((villager) => {
+                    bug5.save()
+                        .then((bug) => {
+                            villager.addItem([bug])
+                        })
+                })
+                    .catch((err) => {
+                        console.log("error saving bug 5");
                     });
                 break;
         }
