b5a1d56f1a9dd8e8deaf7afe4e07045b5a5db9c1
Emily <porte2@mailbox.winthrop.edu>
2020-04-25 22:46:29 -0400

    sell routes added

--- routes/nooks.js
+++ routes/nooks.js
@@ -5,3 +5,75 @@ const sequelize = ('./utils/sequelize');
 const Villager = require('./models/Villager');
 const Item = require('./models/Item');
 const House = require('./models/House');
+
+
+router.get('/', (req, res) => {
+    if (req.session.user) {
+        res.render('nooksCranny/index');
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+router.get('/sell', (req, res) => {
+    if (req.session.user) {
+        Villager.findByPk(req.session.user)
+            .then((villager) => {
+                villager.getItems()
+                    .then((items) => {
+                        res.render('nooksCranny/sell', { villager: villager, items : items });
+                    })
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+router.post('/sell', (req, res) => {
+    let user = req.session.user;
+    let sold = req.body.sold;
+    Villager.findByPk(user)
+    .then((villager) => {
+        Item.findByPk(sold)
+        .then((item) => {
+            let value = item.price;
+            villager.money = villager.money + value;
+            villager.save()
+            .then(() => {
+                item.destroy()
+                .then(() => {
+                    res.redirect('/nooksCranny/');
+                })
+            })
+        })
+    }).catch((err) => {
+        console.log(err);
+    }) 
+});
+
+router.get('/pay', (req, res) => {
+    if (req.session.user) {
+        Villager.findByPk(req.session.user)
+        .then((villager) => {
+            villager.getHouse()
+            .then((house) => {
+                res.render('nooksCranny/pay', {villager : villager, house : house});
+            })
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+router.post('/pay', (req, res) => {
+
+});
+
+module.exports = router;
